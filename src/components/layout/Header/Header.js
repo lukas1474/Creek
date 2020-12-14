39c4df09844dabd9142fbf58d1fb2aae30b559d9
Lukasz Sobel <lukasz.sobel@hotmail.com>
2020-12-14 21:34:57 +0100

    add new styles for header and footer

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -8,23 +8,28 @@ import logo from '../../../photo/creekLOGO.png';
 
 const Header = () => (
   <header className={styles.header}>
-    <Navbar collapseOnSelect expand="lg" bg="white" >
+    <div className={styles.headerTop}/>
+    <div className={styles.logoContainer}>
       <img src={logo} alt='logo' className={styles.logo} />
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">O nas</Nav.Link>
-          <NavDropdown title="Sklep" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Ubrania</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Sprzęt biwakowy</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Akcesoria</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-        <Nav>
-          <Nav.Link href="#deets">Kontakt</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    </div>
+    <div className={styles.navbar}>
+      <Navbar collapseOnSelect expand="lg" bg="#282c34" variant="dark">
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">O nas</Nav.Link>
+            <NavDropdown title="Sklep" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Ubrania</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Sprzęt biwakowy</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Akcesoria</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">Kontakt</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
   </header>
 );
 
