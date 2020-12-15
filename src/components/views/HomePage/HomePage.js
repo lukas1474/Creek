313b789a56982e751b4cf86d8ae2ac02d01539c1
Lukasz Sobel <lukasz.sobel@hotmail.com>
2020-12-15 22:04:31 +0100

    add styles on HomePage

--- src/components/views/HomePage/HomePage.js
+++ src/components/views/HomePage/HomePage.js
@@ -4,37 +4,97 @@ import PropTypes from 'prop-types';
 import styles from './HomePage.module.scss';
 
 import { Link } from 'react-router-dom';
+import { Col, Container, Row } from 'react-bootstrap';
 
 class HomePage extends React.Component {
   render() {
     return (
       <div className={styles.root}>
-        <Link to={`/oNas/`} className={styles.link}>
-          <div>
-            <h2>Ekipa</h2>
-          </div>
-        </Link>
-        <Link to={`/ubrania/`} className={styles.link}>
-          <div>
-            <h2>Ubrania</h2>
-          </div>
-        </Link>
-        <Link to={`/sprzęt/`} className={styles.link}>
-          <div>
-            <h2>Sprzęt biwakowy</h2>
-          </div>
-        </Link>
-        <Link to={`/akcesoria/`} className={styles.link}>
-          <div>
-            <h2>Akcesoria</h2>
-          </div>
-        </Link>
-        <Link to={`/kontakt/`} className={styles.link}>
-          <div>
-            <h2>Kontakt</h2>
-          </div>
-        </Link>
-      </div >
+        <Container >
+          <Row xs={1} md={1} lg={2}>
+            <Link to={`/ubrania/`} className={styles.link}>
+              <Row className={styles.viewSubpages}>
+                <Col className={styles.colImg}>
+                  <img
+                    className={styles.viewSubpagesImg}
+                    width={350}
+                    height={250}
+                    alt='subpagesImg' />
+                </Col>
+                <Col className={styles.viewSubpagesText}>
+                  <div >
+                    <h2>Ubrania</h2>
+                  </div>
+                </Col>
+              </Row>
+            </Link>
+            <Link to={`/sprzęt/`} className={styles.link}>
+              <Row className={styles.viewSubpages}>
+                <Col className={styles.colImg}>
+                  <img
+                    className={styles.viewSubpagesImg}
+                    width={350}
+                    height={250}
+                    alt='subpagesImg' />
+                </Col>
+                <Col className={styles.viewSubpagesText}>
+                  <div>
+                    <h2>Biwak</h2>
+                  </div>
+                </Col>
+              </Row>
+            </Link>
+            <Link to={`/akcesoria/`} className={styles.link}>
+              <Row className={styles.viewSubpages}>
+                <Col className={styles.colImg}>
+                  <img
+                    className={styles.viewSubpagesImg}
+                    width={350}
+                    height={250}
+                    alt='subpagesImg' />
+                </Col>
+                <Col className={styles.viewSubpagesText}>
+                  <div>
+                    <h2>Akcesoria</h2>
+                  </div>
+                </Col>
+              </Row>
+            </Link>
+            <Link to={`/oNas/`} className={styles.link}>
+              <Row className={styles.viewSubpages}>
+                <Col className={styles.colImg}>
+                  <img
+                    className={styles.viewSubpagesImg}
+                    width={350}
+                    height={250}
+                    alt='subpagesImg' />
+                </Col>
+                <Col className={styles.viewSubpagesText}>
+                  <div>
+                    <h2>Nasza drużyna</h2>
+                  </div>
+                </Col>
+              </Row>
+            </Link>
+            <Link to={`/kontakt/`} className={styles.link}>
+              <Row className={styles.viewSubpages}>
+                <Col className={styles.colImg}>
+                  <img
+                    className={styles.viewSubpagesImg}
+                    width={350}
+                    height={250}
+                    alt='subpagesImg' />
+                </Col>
+                <Col className={styles.viewSubpagesText}>
+                  <div>
+                    <h2>Kontakt</h2>
+                  </div>
+                </Col>
+              </Row>
+            </Link>
+          </Row>
+        </Container>
+      </div>
     );
   }
 }
